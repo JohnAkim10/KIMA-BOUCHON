64b59e68649a79965489f53168af86baeb4cad8e
John Akim <johnkayangi@gmail.com>
2021-05-05 00:35:37 +0200

    Mon premier commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kima Bouchon</title>
+</head>
+<body>
+    Ici je présente le service en 2 phrases ou 3 max puis je met un bouton d'appel à l'action
+    <header>
+
+    </header>
+    Section qui est sensée afficher les services que l'entreprise offre et les tarifs avec des appels à l'action.
+    <section>
+        <article>
+            
+        </article>
+        <article>
+            
+        </article>
+        <article>
+            
+        </article>
+    </section>
+    <section>
+
+    </section>
+    <section>
+
+    </section>
+    <section>
+
+    </section>
+    <footer>
+    </footer>
+</body>
+</html>
