328ca0f6ea6bc4752d36ec5060408fe8909467c2
John Akim <johnkayangi@gmail.com>
2021-05-06 04:32:01 +0200

    Implementation de la première partie du design

--- index.php
+++ index.php
@@ -5,12 +5,100 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Kima Bouchon</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <!-- Ici je présente le service en 2 phrases ou 3 max puis  je met un bouton d'appel à l'action -->
-    <header>
-
+    <header class="head">
+        <div class="presentation h-100 container-fluid">
+            <div class="nav">
+                <ul class="row nav-item ml-auto pr-5 mt-3">
+                    <li class="nav-link">Accueil</li>
+                    <li class="nav-link">Situation Routière</li>
+                    <li class="nav-link">Profile</li>
+                </ul>
+            </div>
+            <div class="header-content container mt-5">
+                <h1 class="title">KIMA <br>BOUCHON</h1>
+                <h5 class="description p-2">Tenez-vous au courant de la situation de la circulation dans la ville province de Kinshasa</h5>
+                <a href="#" class="btn btn-rounded bg-light ml-3 mt-3 bouton">TELECHARGER L'APPLICATION MOBILE</a>
+            </div>
+            
+        </div>
     </header>
+    <section class="about">
+        <div class="row container pl-4">
+            <div class="col-md-6 p-5 m-auto" style="">
+                <h1 class="section-title">QUI SOMMES-NOUS</h1><br>
+                <p class="describe">Kima bouchon est une plateforme web et mobile vous permettant de connaitre la situation de la circulation routière à Kinshasa et ce quasiment en temps réelle
+                    <br><br>
+                    Vous trouverez des images prises dans les différents carrefours et des indices sur l'itensité des bouchons afin de vous permettre d'optimiser vos trajets et de gagner en temps
+                </p>
+            </div>
+            <div class="container col-md-6 m-auto" style="height:300px; background:url(./assets/images/logo.png) center; background-size:cover">
+                <!-- <img src="./assets/images/logo.png" alt="" class="bg-dark" height="400px" style="margin-left:0px"> -->
+            </div>
+        </div>
+        
+    </section>
+
+    <section class="type" style="">
+        <div class="container p-5">
+        <h1 class="section-title text-light text-center">TYPES D'ABONNEMENTS</h1><br>
+            <div class="container row m-auto">
+                <div class="card col-md-4 shadow p-3 text-light m-auto" style="background:#FFF3; border-radius:10px; height:400px;">
+                    <h5 class="sub-title text-center">
+                        STANDARD
+                        <hr class="bg-light w-50">
+                    </h5>
+                    <div class="prices text-center">
+                        <div class="container text-center" style="vertical-align:center">
+                            <h1 class="m-auto">2.5$/<span class="duree" style="font-size:18px">2 Semaine</span></h1>
+                        </div>
+                        <div class="container text-center" style="vertical-align:center">
+                            <h1 class="m-auto">5$/<span class="duree" style="font-size:18px">1 Mois</span></h1>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="privileges">
+                        <ul> <b>Privilèges : </b>  <br>
+                            <li class="ml-5">Accès aux images</li>
+                            <li class="ml-5">Accès à la carte Maps</li>
+                        </ul>
+                    </div>
+                    <br>
+                    <a href="#" class="btn btn-rounded bg-light ml-3 mt-5 bouton w-50 m-auto">COMMANDER</a>
+                </div>
+                
+                <div class="card col-md-4 shadow p-3 text-light m-auto" style="background:#FFF3; border-radius:10px; height:400px;">
+                    <h5 class="sub-title text-center">
+                        VIP
+                        <hr class="bg-light w-50">
+                    </h5>
+                    <div class="prices text-center">
+                        <div class="container text-center" style="vertical-align:center">
+                            <h1 class="m-auto">2.5$/<span class="duree" style="font-size:18px">2 Semaine</span></h1>
+                        </div>
+                        <div class="container text-center" style="vertical-align:center">
+                            <h1 class="m-auto">5$/<span class="duree" style="font-size:18px">1 Mois</span></h1>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="privileges">
+                        <ul> <b>Privilèges : </b>  <br>
+                            <li class="ml-5">Accès aux images</li>
+                            <li class="ml-5">Accès aux vidéos</li>
+                            <li class="ml-5">Accès à la carte Maps</li>
+                        </ul>
+                    </div>
+                    <br>
+                    <a href="#" class="btn btn-rounded bg-light ml-3 mt-5 bouton w-50 m-auto">COMMANDER</a>
+                </div>
+            </div>
+        </div>
+        
+    </section>
     <!-- Section qui est sensée afficher les services que l'entreprise offre et les tarifs avec des appels à l'action. -->
     <section>
         <article>
